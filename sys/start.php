c965f7ab730c3f75dfefe395697822ed2782b8f0
DZLZH <dzlzh@null.net>
2016-11-28 15:09:20 +0100

    Defines constants

---
+++ sys/start.php
@@ -0,0 +1,9 @@
+<?php
+//框架启动文件
+define('APP_PATH', ROOT_PATH . 'app' . DS); //定义应用程序目录路径
+define('RUNTIME_PATH', ROOT_PATH . 'runtime' . DS); //定义框架运行时目录路径
+define('CONF_PATH', ROOT_PATH . 'config' .DS); //定义全局配置目录路径
+define('CORE_PATH', ROOT_PATH . 'sys' . DS . 'core' . DS); //定义框架核心目录路径
+
+
+
